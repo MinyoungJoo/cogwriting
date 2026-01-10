f682706ba8e41e5f16e7b63838c1e0052b05acac
MINYOUNG JOO <lilybell77@ewhain.net>
2026-01-11 06:24:44 +0900

    fix : mongodb error

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 interface GlobalMongoose {
     conn: mongoose.Connection | null;
     promise: Promise<mongoose.Connection> | null;
@@ -22,6 +18,10 @@ if (!cached) {
 }
 
 async function dbConnect() {
+    if (!MONGODB_URI) {
+        throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    }
+
     if (cached.conn) {
         return cached.conn;
     }
@@ -31,7 +31,7 @@ async function dbConnect() {
             bufferCommands: false,
         };
 
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose.connection;
         });
     }
